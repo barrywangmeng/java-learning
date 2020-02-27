5e732d45432217345f51481f1b220586f1c5ee14
wangmeng2 <wangmeng2@tuhu.cn>
2020-02-27 11:30:44 +0800

    动手实现arrayList代码

--- java-learning/src/main/java/cn/barrywangmeng/collection/jdk8/MyArrayList.java
+++ java-learning/src/main/java/cn/barrywangmeng/collection/jdk8/MyArrayList.java
@@ -1,5 +1,8 @@
 package cn.barrywangmeng.collection.jdk8;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * 自己实现ArrayList，主要是扩容步骤
  *
@@ -55,15 +58,92 @@ public class MyArrayList<E> {
 
     /**
      * 添加一个元素
-     * @param e 集合中的元素
+     *
+     * @param e 要添加的元素
      * @return 是否添加成功
      */
     public boolean add(E e) {
         // 判断数组是否需要扩容
-//        ensureCapacityInternal(size + 1);
+        ensureCapacityInternal(size + 1);
         // 设置值
         elementData[size++] = e;
         return true;
+    }
+
+    /**
+     * 指定位置插入数据
+     *
+     * @param index 指定的位置
+     * @param element 要添加的元素
+     */
+    public void add(int index, E element) {
+        if (index > size || index < 0) {
+            throw new IndexOutOfBoundsException("Index: "+index+", Size: "+size);
+        }
+
+        ensureCapacityInternal(size +1);
+        // 从原来的element第index位置开始拷贝，拷贝size - index个元素
+        // 到新的element上
+        /**
+         * 比如 elementData[0, 1, 2, 3, 4, 5]  index = 1
+         * System.arraycopy(elementData, 1, elementData, 2, 5);
+         *
+         * 拷贝后变成elementData[0, 1, 1, 2, 3, 4, 5]
+         */
+        System.arraycopy(elementData, index, elementData, index + 1, size - index);
+        elementData[index] = element;
+        size++;
+    }
+
+    /**
+     * 计算是否需要扩容
+     */
+    private void ensureCapacityInternal(int minCapacity) {
+        ensureExplicitCapacity(calculateCapacity(elementData, minCapacity));
+    }
+
+    /**
+     * 判断是否扩容
+     */
+    private void ensureExplicitCapacity(int minCapacity) {
+        if (minCapacity - elementData.length > 0) {
+            // 扩容
+            grow(minCapacity);
+        }
+    }
+
+    /**
+     * 扩容
+     */
+    private void grow(int minCapacity) {
+        // 扩容的原理是使用Arrays.copyOf
+        // 扩容大小为之前容量大小的1.5倍
+        int oldCapacity = elementData.length;
+        // 位操作，右移1位 oldCapacity / 2
+        int newCapacity = oldCapacity + (oldCapacity >> 1);
+        if (newCapacity - minCapacity < 0) {
+            newCapacity = minCapacity;
+        }
+        if (newCapacity - Integer.MAX_VALUE > 0) {
+            throw new OutOfMemoryError();
+        }
+        // 数组拷贝
+        elementData = Arrays.copyOf(elementData, newCapacity);
+    }
+
+    /**
+     * 为数组设置初始化容量
+     * @param elementData 数组 第一次时数组为空
+     * @param minCapacity 数组大小
+     * @return 数组大小
+     */
+    private static int calculateCapacity(Object[] elementData, int minCapacity) {
+        if (elementData == DEFAULT_CAPACITY_EMPTY_ELEMENT_DATA) {
+            return Math.max(DEFAULT_SIZE, minCapacity);
+        }
+
+        // 如果初始化传递的有capacity，则使用传递的capacity
+        return minCapacity;
     }
 
     public static void main(String[] args) {
@@ -73,5 +153,19 @@ public class MyArrayList<E> {
         }
 
         System.out.println(list);
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(0);
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(3);
+        arrayList.add(4);
+        arrayList.add(5);
+        arrayList.add(1, 6);
+        System.out.println("aaa");
+
+        int[] arrays = {0, 1, 2, 3, 4, 5};
+        System.arraycopy(arrays, 1, arrays, 2, 5);
+        System.out.println("aaa");
     }
 }
